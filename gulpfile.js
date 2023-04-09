7c2a63f0b82290a80f5fc000067abfadd7ac1dc2
martaalcazarr <martaalcazarredondo@gmail.com>
2023-04-09 01:05:21 -0400

    css, configuraciones gulp, imagenes + formatos

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const {src, dest, watch, series, parallel} = require('gulp');
+
+//css y sass
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+//imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     //compilar sass
@@ -17,15 +24,43 @@ function css(done){
     done()
 }
 
+function imagenes(){
+    return src('src/img/**/*')
+        .pipe(imagemin({optimizationLevel: 3}))
+        .pipe(dest('build/img'))
+}
+
+function versionWebp(){
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+}
+
+function versionAvif(){
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+}
+
 function dev(){
     //toma dos valores, primero a que archivo tiene que estar atento si pasan cambios
     //y segundo, que funcion ejecutar en ese archivo cuando hay cambios
     watch('src/scss/**/*.scss', css)
+    watch('src/img/**/*', imagenes)
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series(css, dev)
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
 
 //series : inicia una tarea y al finalizar inicia la siguiente
 //parallel: inicia las tareas en paralelo
